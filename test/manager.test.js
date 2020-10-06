a7294f3665f151c7921ce0bac6ed0b6cc16ec273
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-05 21:44:23 -0700

    completed manager.test.js

--- test/manager.test.js
+++ test/manager.test.js
@@ -2,19 +2,13 @@ const Manager = require("../lib/manager");
 const { test, expect } = require("@jest/globals");
 
 // Test that user can set the manager's office number
-test("Can set engineer's GitHub username", () => {
-    const testEmployee = new Manager("testName",1,"janedoe@test.com","testUsername");
-    expect(testEmployee.github).toBe("testUsername");
-});
-
-// Test user can retreive manager's GitHub username by calling the getGithub() function
-test("Can get GitHub username with getGithub() function", () => {
-    const testEmployee = new Manager("testName",1,"janedoe@test.com","testUsername");
-    expect(testEmployee.getGithub()).toBe("testUsername");
+test("Can set manager's office number", () => {
+    const testEmployee = new Manager("testName",1,"janedoe@test.com",101);
+    expect(testEmployee.github).toBe(101);
 });
 
 // Test that calling the getRole() function will now override and return 'Manager'
 test("getRole() function now returns 'Manager'", () => {
-    const testEmployee = new Manager("testName",1,"janedoe@test.com","testUsername");
+    const testEmployee = new Manager("testName",1,"janedoe@test.com",101);
     expect(testEmployee.getRole()).toBe("Manager");
 });
