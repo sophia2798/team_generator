c1f14ebba8d1e5def05e0b5a0d78650b2e07b7f9
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-05 23:12:52 -0700

    wrapped async function differently and added catch

--- app.js
+++ app.js
@@ -8,58 +8,61 @@ const Intern  = require("./lib/intern");
 const Manager = require("./lib/manager");
 const { async } = require("rxjs");
 
-// Wrap in an async function to ensure the correct timing for the inquirer prompts
-async function initialize() {
     console.log("Let's create your development team!");
 
     // Set global variables
     let HTMLString = "";
 
     // Set function that calls main set of questions
-    const mainQuestions = () => {
-        const employeeQ = await inquirer.prompt([
-            {
-                type:"input",
-                message:"Enter the employee's name",
-                name: "name",
-                validate: async (input) => {
-                    if (input === "") {
-                        return "Please input a name"
+    async function mainQuestions() {
+        try {
+            const employeeQ = await inquirer.prompt([
+                {
+                    type:"input",
+                    message:"Enter the employee's name",
+                    name: "name",
+                    validate: async (input) => {
+                        if (input === "") {
+                            return "Please input a name"
+                        }
+                        return true;
                     }
-                    return true;
-                }
-            },
-            {
-                type:"input",
-                message:"Enter the employee's ID",
-                name:"id",
-                validate: async (input) => {
-                    if (isNaN(input)) {
-                        return "You must input a number"
+                },
+                {
+                    type:"input",
+                    message:"Enter the employee's ID",
+                    name:"id",
+                    validate: async (input) => {
+                        if (isNaN(input)) {
+                            return "You must input a number"
+                        }
+                        return true;
                     }
-                    return true;
-                }
-            },
-            {
-                type:"input",
-                message:"Enter the employee's email",
-                type:"email",
-                validate: async (input) => {
-                    if(input.includes("@") === false || input.includes(".") === false) {
-                        return "You must input a valid email"
+                },
+                {
+                    type:"input",
+                    message:"Enter the employee's email",
+                    type:"email",
+                    validate: async (input) => {
+                        if(input.includes("@") === false || input.includes(".") === false) {
+                            return "You must input a valid email"
+                        }
+                        return true;
                     }
-                    return true;
+                },
+                {
+                    type:"list",
+                    message:"Which of the following roles best describes this employee?",
+                    choices:["Engineer","Intern","Manager"],
+                    name:"role"
                 }
-            },
-            {
-                type:"list",
-                message:"Which of the following roles best describes this employee?",
-                choices:["Engineer","Intern","Manager"],
-                name:"role"
-            }
-        ]);
-
+            ]);
+            console.log(employeeQ.role)
+            // Display different set of questions depending on role of employee
+            // if (mainQuestions.role === )
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
-}
-
-initialize();
+    mainQuestions();
