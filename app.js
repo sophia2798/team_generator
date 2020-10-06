1d3de0d10e7198caeef7c2f54173dfa9da695dca
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-05 21:50:36 -0700

    added requirements to engineer, intern, and manager js files

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const jest = require("jest");
+// Team Member .js Files
+const Engineer = require("./lib/engineer");
+const Intern  = require("./lib/intern");
+const Manager = require("./lib/manager");
