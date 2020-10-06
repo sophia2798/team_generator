ce6ce22f7c7c3eae5e2f40acb31125e8d93f1f9e
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-05 23:32:29 -0700

    fixed inquirer bug, added a missing name parameter

--- app.js
+++ app.js
@@ -6,63 +6,58 @@ const jest = require("jest");
 const Engineer = require("./lib/engineer");
 const Intern  = require("./lib/intern");
 const Manager = require("./lib/manager");
-const { async } = require("rxjs");
 
-    console.log("Let's create your development team!");
+console.log("Let's create your development team!");
 
-    // Set global variables
-    let HTMLString = "";
+// Set global variables
+let HTMLString = "";
 
-    // Set function that calls main set of questions
-    async function mainQuestions() {
-        try {
-            const employeeQ = await inquirer.prompt([
-                {
-                    type:"input",
-                    message:"Enter the employee's name",
-                    name: "name",
-                    validate: async (input) => {
-                        if (input === "") {
-                            return "Please input a name"
-                        }
-                        return true;
-                    }
-                },
-                {
-                    type:"input",
-                    message:"Enter the employee's ID",
-                    name:"id",
-                    validate: async (input) => {
-                        if (isNaN(input)) {
-                            return "You must input a number"
-                        }
-                        return true;
-                    }
-                },
-                {
-                    type:"input",
-                    message:"Enter the employee's email",
-                    type:"email",
-                    validate: async (input) => {
-                        if(input.includes("@") === false || input.includes(".") === false) {
-                            return "You must input a valid email"
-                        }
-                        return true;
-                    }
-                },
-                {
-                    type:"list",
-                    message:"Which of the following roles best describes this employee?",
-                    choices:["Engineer","Intern","Manager"],
-                    name:"role"
+// Set function that calls main set of questions
+// Wrap in async function to wait for responses from employee questions before branching out into other role specific questions
+async function mainQuestions() {
+    const employeeQuestions = await inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter the employee's name",
+            name: "name",
+            validate: async (input) => {
+                if (input === "") {
+                    return "Please input a name"
                 }
-            ]);
-            console.log(employeeQ.role)
-            // Display different set of questions depending on role of employee
-            // if (mainQuestions.role === )
+                return true;
+            }
+        },
+        {
+            type:"number",
+            message:"Enter the employee's ID",
+            name:"id",
+            validate: async (input) => {
+                if (isNaN(input)) {
+                    return "You must input a number"
+                }
+                return true;
+            }
+        },
+        {
+            type:"input",
+            message:"Enter the employee's email",
+            name:"email",
+            validate: async (input) => {
+                if(input.includes("@") === false || input.includes(".") === false) {
+                    return "You must input a valid email"
+                }
+                return true;
+            }
+        },
+        {
+            type:"list",
+            message:"Which of the following roles best describes this employee?",
+            choices:["Engineer","Intern","Manager"],
+            name:"role"
         }
-        catch (err) {
-            console.log(err)
-        }
-    }
-    mainQuestions();
+    ]);
+
+    // Display different set of questions depending on role of employee
+    if (mainQuestions.role === )
+}
+mainQuestions();
