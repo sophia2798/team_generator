6cf18dfacf6cc29f9037d8b3390d6ee4e00ae8b7
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-05 21:29:54 -0700

    finished the engineer.js file

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -3,18 +3,18 @@ const { test, expect } = require("@jest/globals");
 
 // Test that user can set the engineer's GitHub username
 test("Can set engineer's GitHub username", () => {
-    const testEmployee = new Engineer("testName",1,"janedoe@test.com","testUsername");
+    const testEmployee = new Engineer("testUsername");
     expect(testEmployee.github).toBe("testUsername");
 });
 
 // Test user can retreive engineer's GitHub username by calling the getGithub() function
 test("Can get GitHub username with getGithub() function", () => {
-    const testEmployee = new Engineer("testName",1,"janedoe@test.com","testUsername");
+    const testEmployee = new Engineer("testUsername");
     expect(testEmployee.getGithub()).toBe("testUsername");
 });
 
 // Test that calling the getRole() function will now override and return 'Engineer'
 test("getRole() function now returns 'Engineer'", () => {
-    const testEmployee = new Engineer("testName",1,"janedoe@test.com","testUsername");
+    const testEmployee = new Engineer("testUsername");
     expect(testEmployee.getRole()).toBe("Engineer");
 });
